48ebda5e57003d4172d2865fceb25e33dc05e182
houssembalti <houssem.balti@esprit.tn>
2022-12-02 01:11:59 +0100

    table+add

--- club-app-front/src/app/app-routing.module.ts
+++ club-app-front/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddmembreComponent } from './membre/addmembre/addmembre.component';
+import { TablemembreComponent } from './membre/tablemembre/tablemembre.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent },
+  {path:'listmembres',component:TablemembreComponent,},
+  {path:'add',component:AddmembreComponent},
+  
 ];
 
 @NgModule({
--- club-app-front/src/app/app.module.ts
+++ club-app-front/src/app/app.module.ts
@@ -6,17 +6,31 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MembreComponent } from './membre/membre.component';
+import { TablemembreComponent } from './membre/tablemembre/tablemembre.component';
+import {HttpClientModule} from '@angular/common/http';
+import { AddmembreComponent } from './membre/addmembre/addmembre.component';
+import { FormsModule } from '@angular/forms';
+import { PipemembrePipe } from './membre/pipemembre.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MembreComponent,
+    TablemembreComponent,
+    AddmembreComponent,
+    PipemembrePipe,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

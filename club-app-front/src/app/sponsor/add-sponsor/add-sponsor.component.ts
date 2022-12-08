1c979a68baf2d2d2b12972132b937cdf3ce84504
ouertaniaziz <mohamedaziz.ouertani@esprit.tn>
2022-12-08 14:45:43 +0100

    ajouter sponsor bd

--- club-app-front/src/app/sponsor/add-sponsor/add-sponsor.component.ts
+++ club-app-front/src/app/sponsor/add-sponsor/add-sponsor.component.ts
@@ -1,3 +1,5 @@
+import { SponsorService } from './../../shared/sponsor.service';
+import { Sponsor } from './../../models/sponsor';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -8,25 +10,39 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AddSponsorComponent implements OnInit {
   myForm :  FormGroup;
-
+sponsor: Sponsor= new Sponsor();
  
-  constructor() { }
+  constructor(private spS:SponsorService) { }
 
   ngOnInit(): void {
+    this.createForm(new Sponsor());
+
+   
+  }
+
+
+  createForm(sponsor){
     this.myForm=new FormGroup({
       
-      email:new FormControl('',[Validators.required,Validators.pattern("^[a-zA-Z0-9._-]+@gmail.com")]),
-      nom : new FormControl('',[Validators.required, Validators.minLength(5), Validators.pattern("[a-zA-Z]*")]),
-      phone: new FormControl('',[Validators.required, Validators.minLength(8),Validators.maxLength(8), Validators.pattern("[0-9]*")]),
-      categorie:new FormControl('', [Validators.required]),
+      email:new FormControl(sponsor.email ,[Validators.required,Validators.pattern("^[a-zA-Z0-9._-]+@gmail.com")]),
+      nom : new FormControl(sponsor.labelle,[Validators.required, Validators.minLength(5), Validators.pattern("[a-zA-Z]*")]),
+      phone: new FormControl(sponsor.phone,[Validators.required, Validators.minLength(8),Validators.maxLength(8), Validators.pattern("[0-9]*")]),
+      categorie:new FormControl(sponsor.categorieSponsor, [Validators.required]),
       })
     
   }
 
 
-  
+save(){
+
+  this.sponsor.email= this.myForm.get('email').value;
+  this.sponsor.labelle= this.myForm.get('nom').value;
+  this.sponsor.categorieSponsor=this.myForm.get('categorie').value;
+this.spS.addsponsor(this.sponsor).subscribe();
+
+this.myForm.reset();
 
 
-save(){}
+}
   
 }
